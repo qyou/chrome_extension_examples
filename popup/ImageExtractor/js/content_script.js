5802504f0236f07e02d94e55db4714988bccfe8e
qyou <qyou.casia@gmail.com>
2014-09-08 16:02:55 +0800

    image constrains

--- popup/ImageExtractor/js/content_script.js
+++ popup/ImageExtractor/js/content_script.js
@@ -7,9 +7,19 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
       }
       else {
         var imgsrc = [];
+        var j=0;
         for (var i=0; i<images.length; i++) {
-            imgsrc[i] = images[i].src;
+			
+			if (images[i].width > 100 && images[i].height > 100) {
+			   var imgurl = images[i].src;
+				if (['jpg', 'png', 'gif', 'bmp', 'tif'].indexOf(imgurl.substr(imgurl.lastIndexOf('.')+1).toLowerCase()) != -1) {
+					imgsrc[j] = imgurl;
+					j++;
+				}
+			}
         }
+        
+        
         sendResponse({'type':1, 'returnval': imgsrc});
       }
       console.log("send message...");
